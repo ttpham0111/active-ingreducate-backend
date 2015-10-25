1ee522db8b16d0144641b716b9ef8b378c34021f
Tuan Pham <tt.pham1991+git@gmail.com>
2015-10-24 21:41:16 -0700

    Updated imageParser with new parsing algorithm.

--- routes/imageParser.js
+++ routes/imageParser.js
@@ -46,28 +46,52 @@ module.exports = function(app)
 
           var result = 'No ingredients found';
 
-          // Food ingredients list.
-          if (text.toLowerCase().indexOf('ingredients') > -1)
-          {
-            text = text.replace('/.*:(.*)/', '$1');
-            var ingredients = text.split(',');
-            for (var ingredient in ingredients)
-            {
-              ingredient = ingredient.trim();
-            }
-            result = ingredients;
-          }
           // Drugs active ingredients list.
-          else if (text.indexOf('...') > -1)
+          if (text.indexOf('...') > -1)
           {
-            var activeIngredients = text.replace('/^\s*(.*)\.\.+/mg', '$1');
-            for (var activeIngredient in activeIngredients)
+            var activeIngredients = text.match(/^.*\.\.\.+/gm);
+
+            for (var i = 0; i < activeIngredients.length; ++i)
             {
-              activeIngredient = activeIngredient.trim();
+              activeIngredients[i] = activeIngredients[i]
+              .replace(/^(.*?)\.\.\.+/, '$1')
+              .replace(/\n+/g, ' ')
+              .replace(/\s+/g, ' ')
+              .trim();
+
+              console.log(i + ' ' + activeIngredients[i] + '\n');
             }
             result = activeIngredients;
           }
-          res.send(null, result);
+
+          // Food ingredients list.
+          else if (text.toLowerCase().indexOf('ingredient') > -1)
+          {
+            text = text.replace(/.*:(.*)/, '$1');
+
+            var ingredients = text.split(',');
+            if (!ingredients.length)
+            {
+              ingredients = text.split('/');
+            }
+
+            for (var j = 0; j < ingredients.length; ++j)
+            {
+              ingredients[j] = ingredients[j]
+                .replace(/\n+/g, ' ')
+                .replace(/\s+/g, ' ')
+                .trim();
+
+              console.log(j + ' ' + ingredients[j] + '\n');
+            }
+            result = ingredients;
+          }
+          res.send(
+          {
+            code: 200,
+            message: 'Here are all the ingredients.',
+            result: result
+          });
         });
       });
     });
